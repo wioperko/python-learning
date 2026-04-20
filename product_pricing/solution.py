1a66d2007aa0b85d2157e6413920e94905de9234
Wiola <wioletaperkowska@gmail.com>
2026-04-20 12:44:58 +0200

    Add ProductService and finalize pricing calculation

--- product_pricing/solution.py
+++ product_pricing/solution.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from decimal import Decimal
 from typing import ClassVar, override
+from collections import defaultdict
+
 
 
 @dataclass
@@ -63,17 +65,50 @@ class ExtraProduct(Product):
             f"discount: {self.discount}, discount days: {self.discount_days})"
 
 
+class ProductService:
+
+    def __init__(self):
+        self.products_by_day = defaultdict(list[Product])
+
+    def add_product(self, product: Product, day: int) -> None:
+        if not (0 <= day <= 6):
+            raise ValueError("Day must be between 0 and 6")
+        self.products_by_day[day].append(product)
+
+    def calc_total_price(self) -> Decimal:
+        total_price = Decimal('0.0')
+        for day, products in self.products_by_day.items():
+            for product in products:
+                    if isinstance(product, ExtraProduct):
+                        product.set_current_day(day)
+                    total_price += product.final_price()
+        return total_price
+    
+    def __str__(self) -> str:
+        lines = ["Products:"]
+        for day, products in sorted(self.products_by_day.items()):
+            lines.append(f"Day {day}:")
+            for p in products:
+                lines.append(f"  - {p}")
+        return "\n".join(lines)
+
+
 def main():
     try:
-        # p1 = Product('Face cleanser', 'DRUG', Decimal('100'), Decimal('0.1'))
-        # print(p1)
-        # print(p1.final_price())
 
-        ep1 = ExtraProduct('PA', 'A', Decimal('100'), Decimal('0.1'), {0, 3}, Decimal('0.05'))
-        print(ep1)
-        print(ep1.final_price())
-        ep1.set_current_day(3)
-        print(ep1.final_price())
+        p1 = Product('PA1', 'A', Decimal('100'), Decimal('0.0'))
+        p2 = Product('PA2', 'A', Decimal('100'), Decimal('0.0'))
+        ep1 = ExtraProduct('PB1', 'B', Decimal('100'), Decimal('0.1'), {0, 1}, Decimal('0.05'))
+        ep2 = ExtraProduct('PB2', 'B', Decimal('100'), Decimal('0.1'), {2, 3}, Decimal('0.05'))
+
+        productservice = ProductService()
+        productservice.add_product(p1, 0)
+        productservice.add_product(p2, 0)
+        productservice.add_product(ep1, 1)
+        productservice.add_product(ep2, 1)
+        print(productservice)
+        print(productservice.calc_total_price())
+     
     except Exception as e:
        print(e.args[0])
 
