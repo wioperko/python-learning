b7af855e75c533b2b374b01a67cd17430e3d236d
Wiola <wioletaperkowska@gmail.com>
2026-07-06 22:33:56 +0200

    Add __repr__ method and main function for testing

--- meeting-scheduler/solution.py
+++ meeting-scheduler/solution.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime, date, time
+import pytz
 @dataclass
 class Meeting:
     meeting_id: int
@@ -18,3 +19,27 @@ class Meeting:
     @property
     def is_upcoming(self) -> bool:
         return self.meeting_datetime > datetime.now(self.meeting_datetime.tzinfo)
+    
+    def __repr__(self):
+        return f'Meeting(ID={self.meeting_id}, DATETIME={self.meeting_datetime}, DESCRIPTION={self.description})'
+    
+def main() -> None:
+    m1 = Meeting(1, 'Project kickoff', datetime(2026, 7, 6, 23, 0, tzinfo=pytz.timezone('Europe/Warsaw')))
+    m2 = Meeting(2, 'Team standup', datetime(2026, 8, 28, 10, 0, tzinfo=pytz.timezone('America/New_York')))
+    m3 = Meeting(3, 'Client presentation', datetime(2026, 8, 28, 14, 0, tzinfo=pytz.timezone('Europe/London')))
+    m4 = Meeting(4, 'Retrospective', datetime(2026, 8, 27, 15, 0, tzinfo=pytz.timezone('Asia/Tokyo')))
+    m5 = Meeting(5, 'Team sync', datetime(2026, 8, 29, 11, 0, tzinfo=pytz.timezone('Australia/Sydney')))
+
+    print(m1)
+    print(m2)
+    print(m3)
+    print(m4)
+    print(m5)
+
+    print(m1.has_occurred)
+    print(m1.is_today)
+    print(m1.is_upcoming)
+
+
+if __name__ =='__main__':
+    main()
